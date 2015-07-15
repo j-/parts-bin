f28a8af7a6cc85cb61514194fdd7d946a44232fd
j- <jh@live.com.au>
2015-07-16 00:05:51 +1000

    Added basic editors

---
+++ scripts/bin.js
@@ -0,0 +1,54 @@
+(function (root) {
+
+'use strict';
+
+var bin = {};
+
+bin.frame = document.getElementById('bin');
+bin.window = bin.frame.contentWindow;
+
+bin.reload = function reload (execute) {
+	bin.window.location.reload();
+	// Set immediate
+	setTimeout(execute, 0);
+};
+
+bin.loadContent = function loadContent (content) {
+	if (!content) return;
+	var type = content.type;
+	// Dereference content
+	content = content.content;
+	switch (type) {
+		case 'html': return bin.loadHTML(content);
+		case 'css': return bin.loadCSS(content);
+		case 'js': return bin.loadJavaScript(content);
+	}
+};
+
+bin.loadHTML = function loadHTML (content) {
+	// Create temporary parent container
+	var temp = document.createElement('div');
+	temp.innerHTML = content;
+	var children = temp.childNodes;
+	var frag = document.createDocumentFragment();
+	while (children.length) {
+		frag.appendChild(children[0]);
+	}
+	bin.window.document.body.appendChild(frag);
+};
+
+bin.loadCSS = function loadCSS (content) {
+	var style = document.createElement('style');
+	style.innerHTML = content;
+	bin.window.document.head.appendChild(style);
+};
+
+bin.loadJavaScript = function loadJavaScript (content) {
+	var script = document.createElement('script');
+	script.innerHTML = content;
+	bin.window.document.head.appendChild(script);
+};
+
+root.bin = bin;
+
+})(this);
---
+++ scripts/editor.js
@@ -0,0 +1,57 @@
+(function () {
+
+/* global ace, bin */
+
+'use strict';
+
+var KEY_JS = 'edit-js-value';
+var KEY_HTML = 'edit-html-value';
+var KEY_CSS = 'edit-css-value';
+
+var editJS = ace.edit('edit-js');
+editJS.getSession().setMode('ace/mode/javascript');
+editJS.setValue(localStorage.getItem(KEY_JS));
+editJS.selection.clearSelection();
+
+var editHTML = ace.edit('edit-html');
+editHTML.getSession().setMode('ace/mode/html');
+editHTML.setValue(localStorage.getItem(KEY_HTML));
+editHTML.selection.clearSelection();
+
+var editCSS = ace.edit('edit-css');
+editCSS.getSession().setMode('ace/mode/css');
+editCSS.setValue(localStorage.getItem(KEY_CSS));
+editCSS.selection.clearSelection();
+
+function execute () {
+	var js = editJS.getValue();
+	var html = editHTML.getValue();
+	var css = editCSS.getValue();
+	localStorage.setItem(KEY_JS, js);
+	localStorage.setItem(KEY_HTML, html);
+	localStorage.setItem(KEY_CSS, css);
+	bin.reload(function () {
+		bin.loadHTML(html);
+		bin.loadCSS(css);
+		// Execute in next loop to clear call stack
+		setTimeout(bin.loadJavaScript, 0, js);
+	});
+}
+
+var config = {
+	name: 'execute',
+	bindKey: {
+		win: 'Ctrl-Enter',
+		mac: 'Command-Enter'
+	},
+	exec: execute,
+	readOnly: false
+};
+
+editJS.commands.addCommand(config);
+editHTML.commands.addCommand(config);
+editCSS.commands.addCommand(config);
+
+execute();
+
+})();
